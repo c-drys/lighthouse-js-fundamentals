346ffa7fc326e6e97cf40187d2747e763d02e21e
drystar <chris.drysdale12@gmail.com>
2019-09-26 00:22:22 +0000

    hello world

--- hello.js
+++ hello.js
@@ -7,3 +7,18 @@ sayHello("Ferdinand");
 
 sayHello();
 
+const sayHelloToConsole  = function (name) {
+  console.log("Hello, " + name);
+}
+sayHelloToConsole('John'); 
+
+const returnSayHello  = function (name) {
+  return "Hello, " + name;
+}
+const greeting = returnSayHello('John');
+
+const returnSayHello  = function (name) {
+  return "Hello, " + name;
+}
+const greeting = returnSayHello('John');
+console.log(greeting);
